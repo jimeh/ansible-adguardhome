8f1ed8aa02349773b1943cced74fcea375edcb64
Jim Myhrberg <contact@jimeh.me>
2020-01-18 02:13:55 +0000

    test: Add with-config and change-to-non-root-user test scenarios
    Also run test scenarios in parallel on GitHub Actions.

---
+++ molecule/change-to-non-root-user/tests/test_default.py
@@ -0,0 +1,65 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_adguardhome_config_dir(host):
+    d = host.file('/opt/adguardhome/config')
+
+    assert d.exists
+    assert d.is_directory
+    assert d.mode == 0o755
+    assert d.user == 'adguard'
+    assert d.group == 'adguard'
+
+
+def test_adguardhome_config_file(host):
+    f = host.file('/opt/adguardhome/config/AdGuardHome.yml')
+
+    assert f.exists
+    assert f.is_file
+    assert f.mode == 0o644
+    assert f.user == 'adguard'
+    assert f.group == 'adguard'
+
+
+def test_adguardhome_data_dir(host):
+    d = host.file('/opt/adguardhome')
+
+    assert d.exists
+    assert d.is_directory
+    assert d.mode == 0o755
+    assert d.user == 'adguard'
+    assert d.group == 'adguard'
+
+
+def test_adguardhome_binary(host):
+    f = host.file("/opt/adguardhome/bin/AdGuardHome")
+
+    assert f.exists
+    assert f.is_file
+    assert f.mode == 0o755
+    assert f.user == 'adguard'
+    assert f.group == 'adguard'
+
+
+def test_adguardhome_service(host):
+    s = host.service('adguardhome')
+
+    assert s.is_enabled
+    assert s.is_running
+
+
+def test_adguardhome_http_service(host):
+    socket = host.socket('tcp://127.0.0.1:80')
+
+    assert socket.is_listening
+
+
+def test_adguardhome_dns_service(host):
+    socket = host.socket('tcp://127.0.0.1:53')
+
+    assert socket.is_listening
